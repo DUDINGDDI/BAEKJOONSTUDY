4c314b530e6c4732a374a088ce2da1ec057d568f
MIKROKosMoS <102864363+DUDINGDDI@users.noreply.github.com>
2023-02-27 16:29:41 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2348 ms, Memory: 190412 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+//Conparable
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<PairX> pair = new ArrayList<>();
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			pair.add(new PairX(a, b));
+		}
+		MyComparatorX mc = new MyComparatorX();
+		Collections.sort(pair, mc);
+		for (int i = 0; i < n; i++) {
+			System.out.println(pair.get(i).age+" "+pair.get(i).y);
+		}
+	}
+
+}
+
+class PairX {
+	PairX(int age, int y) {
+		this.age = age;
+		this.y = y;
+	}
+
+	int age;
+	int y;
+}
+
+class MyComparatorX implements
+Comparator<PairX> {
+	@Override
+	public int compare(PairX p1, PairX p2) {
+		if (p1.age > p2.age) {
+			return 1; // x에 대해서는 오름차순
+		} else if (p1.age == p2.age) {
+			if(p1.y>p2.y)
+				return 1;// y에 대해서는 내림차순
+		}
+
+		return -1;
+	}
+}
