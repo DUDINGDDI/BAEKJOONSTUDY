448f74baec69023ea34491617676873a003e0180
MIKROKosMoS <102864363+DUDINGDDI@users.noreply.github.com>
2023-03-20 23:47:46 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int sum = 0;
+//		for(int i = 0; i<5; i++) {
+//			int n = sc.nextInt();
+//			double a = Math.pow(n, 2);
+//			sum+=a;
+//		}
+		System.out.println("Hello World!");
+	}
+}
