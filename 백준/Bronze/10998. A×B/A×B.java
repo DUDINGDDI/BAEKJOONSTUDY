d4d720061e6f56ea4e283632e9001a2d09a2e70a
MIKROKosMoS <102864363+DUDINGDDI@users.noreply.github.com>
2023-03-21 11:08:26 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		System.out.println(n * x);
+	}
+}
