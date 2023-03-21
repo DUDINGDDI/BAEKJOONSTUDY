16f0b41fccfb0b44c8f9d10f5983e0daa29d00fe
MIKROKosMoS <102864363+DUDINGDDI@users.noreply.github.com>
2023-03-21 09:18:15 +0900

    [Bronze III] Title: 알람 시계, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h =sc.nextInt();
+		int m =sc.nextInt();
+		
+		if(m<45) {
+//			h--;
+			if(h==0) {
+				h = 23;
+			}else {
+				h--;
+			}
+			m+=(60-45);
+		}else {
+			m-=45;
+		}
+		System.out.println(h+" "+m);
+	}
+}
