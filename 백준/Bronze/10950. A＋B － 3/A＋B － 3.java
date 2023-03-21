dbb4e3c5ce98cd1947b42b80d27f9d716f341eec
MIKROKosMoS <102864363+DUDINGDDI@users.noreply.github.com>
2023-03-21 10:44:12 +0900

    [Bronze V] Title: A+B - 3, Time: 240 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		for(int t = 0; t<tc; t++) {
+//		StringBuilder sb = new StringBuilder();
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+//		for(int i = 0; i<n; i++) {
+//			int a = sc.nextInt();
+//			if(a<x) {
+//				sb.append(a).append(" ");
+//			}
+//		}
+		System.out.println(n+x);
+		}
+	}
+}
