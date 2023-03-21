e3bfcf3874de19520b83ff7180e493a1fecc441e
MIKROKosMoS <102864363+DUDINGDDI@users.noreply.github.com>
2023-03-21 10:40:31 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		System.out.println(n+m);
+		System.out.println(n-m);
+		System.out.println(n*m);
+		System.out.println(n/m);
+		System.out.println(n%m);
+	}
+}
