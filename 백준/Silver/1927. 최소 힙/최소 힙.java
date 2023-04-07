cc4526a7aaa1ea9e952a70c4b0c303d8739e94eb
MIKROKosMoS <102864363+DUDINGDDI@users.noreply.github.com>
2023-04-07 17:23:48 +0900

    [Silver II] Title: 최소 힙, Time: 1856 ms, Memory: 108688 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,22 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			int x = sc.nextInt();
+			if (x != 0) {
+				pq.add(x);
+			} else {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
+			}
+		}
+	}
+}
