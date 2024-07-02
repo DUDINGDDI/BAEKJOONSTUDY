1f6691b49febbd4c103b70912fecb40abe5d26fc
MIKROKosMoS <102864363+DUDINGDDI@users.noreply.github.com>
2024-07-02 23:44:44 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,6 @@
 class Solution {
-    public int answer;
-    public boolean[] visited;
-
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(0, k, dungeons);
+        int answer = -1;
         return answer;
-    }
-
-    public void dfs(int depth, int k, int[][] dungeons) {
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && dungeons[i][0] <= k) {
-                visited[i] = true;
-                dfs(depth + 1, k - dungeons[i][1], dungeons);
-                visited[i] = false;
-            }
-        }
-        
-        answer = Math.max(answer, depth);
     }
 }
